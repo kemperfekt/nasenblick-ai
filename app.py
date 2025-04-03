5320ca09193d86eb404f8617f09d45388b079b5c
kemperfekt <pk@kemperfekt.de>
2025-04-03 16:30:16 +0200

    Update app.py
    Update

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ except LookupError:
 
 import streamlit as st
 import openai
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, ServiceContext, GPTVectorStoreIndex
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
 from llama_index.llms.openai import OpenAI
 
 # API-Key aus Streamlit Secrets
